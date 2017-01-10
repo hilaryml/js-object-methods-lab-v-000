18a35866e2f66ebb7f222853797061b68971b331
Hilary Lyttle <hilyttle@gmail.com>
2017-01-10 21:15:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+
+  veto() {
+    return "NO!";
+  }
+
+  passBill() {
+    return "You can do that!";
+  }
+
+  doCharity() {
+    return "I like to help people.";
+  }
+
+  conductPressInterview() {
+    return "I am proud to be an American.";
+  }
+
+  sayHi() {
+    return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + ".";
+  }
+}
+
+var georgeWashington = new President("George Washington", "None", "1789 - 1797", "Virginia");
+var abrahamLincoln = new President("Abraham Lincoln", "Republican", "1861 - 1865", "Kentucky");
+var richardNixon = new President("Richard Nixon", "Republican", "1969 - 1974", "California");
+var jimmyCarter = new President("Jimmy Carter", "Democratic", "1977 - 1981", "Georgia");
